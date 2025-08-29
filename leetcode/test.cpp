70508b23da9240bc0f5f81e4e372763690cefbbd
sxq12320 <3383615693@qq.com>
2025-08-29 09:44:29 +0800

    重新配置C++

--- leetcode/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-
-int main()
-{
-    for (int i = 0; i < 5; i++){
-        printf("Hello Grayson~%d\n", i); 
-    }
-    return 0;
-}
---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+
+int main()
+{
+    int n = 0;
+    scanf("%d",&n);
+    printf("%d\n",&n);
+    return 0;
+}
